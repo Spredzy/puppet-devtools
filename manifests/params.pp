00fd71eed7f21a9cd10a147ef5c35fd0fb6bdb39
Yanis Guenane <yguenane@gmail.com>
2012-12-05 18:48:52 +0100

    OS compatibility fix

--- manifests/params.pp
+++ manifests/params.pp
@@ -4,10 +4,10 @@ class devtools::params () {
 
   case $::osfamily {
     'RedHat': {
-      $packages = ['gettext', 'make', 'gcc', 'gcc-c++', 'openssl-devel', 'libicu-devel', 'libyaml-devel', 'zlib-devel', 'readline-devel', 'autoconf', 'perl-ExtUtils-MakeMaker', 'xinetd']
+      $packages = ['gettext', 'make', 'gcc', 'gcc-c++', 'openssl-devel', 'libicu-devel', 'libyaml-devel', 'zlib-devel', 'readline-devel', 'autoconf', 'perl-ExtUtils-MakeMaker']
     }
     'Debian': {
-      $packages = ['gettext', 'make', 'gcc', 'g++', 'libssl-dev', 'curl', 'libicu-dev', 'libyaml-dev', 'zlib1g-dev', 'libreadline-dev', 'autoconf', 'libmodule-build-perl', 'libmodule-install-perl', 'xinetd']
+      $packages = ['gettext', 'make', 'gcc', 'g++', 'libssl-dev', 'curl', 'libicu-dev', 'libyaml-dev', 'zlib1g-dev', 'libreadline-dev', 'autoconf', 'libmodule-build-perl', 'libmodule-install-perl']
     }
     default: {
       fail("Unsupported OS : $::osfamily - Get in touch with the Module maintainer to see how we can fix that")
